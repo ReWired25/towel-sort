b3e6412c3f03ac4836e730415230c2b280eb8b3c
Nikita K <w.feelingalright.p@yandex.ru>
2022-02-03 18:39:53 +0300

    feat: add complete algotithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let finalArr = [];
+
+  if (matrix === undefined) {
+      return [];
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+      for(let k = 0; k < matrix[i].length; k++) {
+          if (i % 2 === 0) {
+              finalArr.push(matrix[i][k])
+          } else {
+              finalArr.push(matrix[i][matrix[i].length - 1 - k])
+          }
+      }
+  }
+
+  return finalArr;
 }
